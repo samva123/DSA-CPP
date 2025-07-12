c6791cc24a0b957d6983c41c73cff6cd0d8bbe08
samva123 <navvarunb@gmail.com>
2025-07-12 16:13:30 +0530

    files added

---
+++ Codes/BACKEND/Class/Lecture_5/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
