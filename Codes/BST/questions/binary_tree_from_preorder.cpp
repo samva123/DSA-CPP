c654e71969bc017ea2af05fde8ec26df63e1e654
samva123 <navvarunb@gmail.com>
2025-10-26 08:14:21 +0530

    error removed from preorder question

--- Codes/BST/questions/binary_tree_from_preorder.cpp
+++ Codes/BST/questions/binary_tree_from_preorder.cpp
@@ -39,19 +39,19 @@ public:
 
 
 
-//////brute force 
+//////brute force in java 
 
-public TreeNode bstFromPreorder(int[] preorder) {
-    TreeNode root = null;
-    for (int val : preorder) {
-        root = insert(root, val);
-    }
-    return root;
-}
+// public TreeNode bstFromPreorder(int[] preorder) {
+//     TreeNode root = null;
+//     for (int val : preorder) {
+//         root = insert(root, val);
+//     }
+//     return root;
+// }
 
-private TreeNode insert(TreeNode root, int val) {
-    if (root == null) return new TreeNode(val);
-    if (val < root.val) root.left = insert(root.left, val);
-    else root.right = insert(root.right, val);
-    return root;
-}
+// private TreeNode insert(TreeNode root, int val) {
+//     if (root == null) return new TreeNode(val);
+//     if (val < root.val) root.left = insert(root.left, val);
+//     else root.right = insert(root.right, val);
+//     return root;
+// }
