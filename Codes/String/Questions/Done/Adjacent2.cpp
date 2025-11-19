94da505c19bfa9e8b61b43fc12ad67bbedf26f84
samva123 <navvarunb@gmail.com>
2025-11-19 23:21:44 +0530

    string completed

--- Codes/String/Questions/Done/Adjacent2.cpp
+++ Codes/String/Questions/Done/Adjacent2.cpp
@@ -1,51 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
-public:
-    bool charsame(string &ans, char &newch, int k_1)
-    {
-        int it = ans.size() - 1;
-        for (int i = 0; i < k_1; ++i)
-        {
-            if (newch != ans[it])
-            {
-                return false;
-            }
-            it--;
-        }
-        return true;
-    }
-    string removeDuplicates(string s, int k)
-    {
-        string ans;
-        for (int i = 0; i < s.size(); ++i)
-        {
-            char &newch = s[i];
-            if (ans.size() < k - 1)
-            {
-                ans.push_back(newch);
-            }
-            else
-            {
-                if (charsame(ans, newch, k - 1))
-                {
-                    for (int j = 0; j < k - 1; ++j)
-                    {
-                        ans.pop_back();
-                    }
-                }
 
-                else
-                {
-                    ans.push_back(newch);
+string removeDuplicates(string s, int k) {
+    bool changed = true;
+
+    while (changed) {
+        changed = false;
+
+        for (int i = 0; i + k <= s.size(); i++) {
+            bool ok = true;
+
+            // check if k consecutive are equal
+            for (int j = 1; j < k; j++) {
+                if (s[i] != s[i + j]) {
+                    ok = false;
+                    break;
                 }
             }
+
+            // remove them
+            if (ok) {
+                s.erase(i, k);
+                changed = true;
+                break;  // must restart scanning
+            }
         }
-        return ans;
     }
-};
+
+    return s;
+}
+
 
 class Solution
 {
--- Codes/String/Questions/Done/longest_prefix.cpp
+++ Codes/String/Questions/Done/longest_prefix.cpp
@@ -52,78 +52,26 @@ public:
 
 
 
+#include<bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0][i];
-            for (int j = 1; j < strs.size(); j++) {
-                if (i >= strs[j].length() || strs[j][i] != c)
-                    return strs[0].substr(0, i);
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++) {
+            int j = 0;
+            while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+                j++;
             }
+            prefix = prefix.substr(0, j);  // shorten prefix
+
+            if (prefix == "") break;  // no need to continue
         }
-        return strs[0];
+
+        return prefix;
     }
 };
-
-
-
-// public class Solution {
-//     public String longestCommonPrefix(String[] strs) {
-//         if (strs == null || strs.length == 0) return "";
-
-//         StringBuilder ans = new StringBuilder();
-//         int minLength = strs[0].length();
-
-//         for (int i = 0; i < minLength; i++) {
-//             char currChar = strs[0].charAt(i);
-
-//             for (int j = 1; j < strs.length; j++) {
-//                 // If index i is out of bounds or characters don't match
-//                 if (i >= strs[j].length() || strs[j].charAt(i) != currChar) {
-//                     return ans.toString();
-//                 }
-//             }
-
-//             ans.append(currChar);
-//         }
-
-//         return ans.toString();
-//     }
-// }
-
-
-
-
-
-// ```
-
-// ---
-
-// ### **Complexity of Vertical Scanning**
-
-// 1. **Time Complexity**:
-//    - In the worst case, every character of every string is compared.
-//    - \(O(n \cdot k)\), where \(n\) is the number of strings and \(k\) is the length of the shortest string.
-
-// 2. **Space Complexity**:
-//    - Only a few variables and the result string are used.
-//    - **Space Complexity**: \(O(1)\).
-
-// ---
-
-// ### **Comparison**
-
-// | **Method**              | **Time Complexity**   | **Space Complexity** | **Notes**                                       |
-// |--------------------------|-----------------------|-----------------------|------------------------------------------------|
-// | Sorting (Method 1)       | \(O(nlog n + k)\)   | \(O(1)\)              | Includes sorting overhead but is simple.      |
-// | Iterative Prefix (Method 2) | \(O(n.k)\)      | \(O(k)\)              | No sorting but modifies prefix iteratively.   |
-// | Vertical Scanning        | \(O(n.k)\)      | \(O(1)\)              | Most efficient and avoids unnecessary sorting.|
-
-// ---
-
-// ### **Conclusion**
-
-// The **Vertical Scanning** approach is the most optimized in terms of both time and space complexity. It eliminates the overhead of sorting (Method 1) and avoids unnecessary prefix modifications (Method 2).
